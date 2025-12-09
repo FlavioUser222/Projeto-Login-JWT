2b3ea999e2ff9c67e3cb1ea67363f72cec902626
FlavioUser222 <flavinhoth12@gmail.com>
2025-12-09 13:43:20 -0300

    a

--- vite-project/src/components/cardServicos.jsx
+++ vite-project/src/components/cardServicos.jsx
@@ -7,8 +7,10 @@ export default function CardServicos({ nome, preco, tempo }) {
             <div className="img-card"></div>
             <div className="card-text">
                 <h1>{nome}</h1>
-                <p>R${preco}</p>
-                <p>{tempo} minutos</p>
+                <div className="wrapper-values">
+                    <p>R${preco}</p>
+                    <p>{tempo} minutos</p>
+                </div>
             </div>
         </div>
     )
