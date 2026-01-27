8b7c7e39607ac1ca487e7b20971b9120db943655
Flavio Eduardo Frois Campos <1025198@senacminas.edu.br>
2026-01-27 14:53:21 -0300

    teste

--- backend/routes/agendamentosRoute.js
+++ backend/routes/agendamentosRoute.js
@@ -5,14 +5,16 @@ const {
   changeStatusAgendamentos,
   deleteAgendamento,
   getAgendamentosWP,
+  meusAgendametos,
 } = require("../controllers/agendamentosController");
-const { listarAgendamentosByUser } = require("../repos/AgendamentosRepository");
+
+
 
 async function agendamentosRoute(app) {
   app.get("/agendamentos", getAgendamento);
   app.get("/agendamentoswp", getAgendamentosWP);
   app.get("/totalAgendamentos", getTotalAgendamentos);
-  app.get('/userAgendamento/:cliente_id',listarAgendamentosByUser)
+  app.get('/userAgendamento/:cliente_id',meusAgendametos)
   app.post("/agendamento", postAgendamentoController);
   app.patch("/agendamento/status/:id", changeStatusAgendamentos);
   app.delete("/agendamentos/:id", deleteAgendamento);
